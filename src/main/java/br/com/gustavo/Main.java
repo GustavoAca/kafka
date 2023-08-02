3d84bd9804905039d9be5054b51180f145e95641
Gustavo Acacio <galasdalas50@gmail.com>
2023-08-02 16:42:01 -0300

    finalizado

--- src/main/java/br/com/gustavo/Main.java
+++ src/main/java/br/com/gustavo/Main.java
@@ -1,41 +1,25 @@
 package br.com.gustavo;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Properties;
+import java.math.BigDecimal;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(propertires());
-        String value = "132123,76533,8472321231";
-        ProducerRecord<String, String> record = new ProducerRecord<>("ECOMMERCE_NEW_ORDEM",value,value);
-        Callback getCallback = (data, ex) -> {
-                if (ex != null) {
-                    ex.printStackTrace();
-                    return;
+        try(var orderDispatcher = new KafkaDispatche<Order>()){
+            try(var emailDispatcher = new KafkaDispatche<Email>()) {
+                for (var i = 0; i < 10; i++) {
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
+                    var order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, order);
+
+                    var email = new Email("Tanks","Thank you for your order! We are processing your order");
+                    emailDispatcher.send("ECOMMERCE_NEW_ORDER", userId, email);
                 }
-                System.out.println("Sucesso enviado " + data.topic() + ":::: partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-            };
-
-        var email = "Thank you for your order! We are processing your order";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email,email);
-
-        producer.send(record, getCallback).get();
-        producer.send(emailRecord, getCallback).get();
-    }
-
-
-    private static Properties propertires() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
+            }
+        }
     }
 }
